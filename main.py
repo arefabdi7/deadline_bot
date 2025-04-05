2ccca421db6fc91374e0bbfabda02d7b6d0ce969
ArefAbdi <117046442+arefabdi7@users.noreply.github.com>
2025-04-05 21:02:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -194,4 +194,6 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
+    if __name__ == "__main__":
+    download_calendar("arefabdi", "Arefabdi1382", 1517579904)
     asyncio.run(main())
